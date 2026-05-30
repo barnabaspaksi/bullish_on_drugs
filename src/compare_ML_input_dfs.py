97952094d3cf2ffeb9c39e28840345a2c9a0aac4
barnabas.paksi <barnabas.paksi@zeiss.com>
2026-05-30 11:43:21 +0200

    wastewater full comparison added (passed)

--- src/compare_ML_input_dfs.py
+++ src/compare_ML_input_dfs.py
@@ -2,16 +2,20 @@ import pandas as pd
 
 ww = pd.read_csv("../data/processed/euda_wastewater_2011_2024_3NF.csv").drop("Unnamed: 0", axis = 1)
 ww["metabolite_name"] = ww["metabolite_name"].str.lower()
-ww_ref = pd.read_csv("C:/Users/Bhar2/Downloads/Part1/Part1/Code/wastewater_3NF_reference.csv")
+ww_ref = pd.read_csv("../data/processed/wastewater_3NF_reference.csv")
 ww_ref["metabolite_name"] = ww_ref["metabolite_name"].str.lower()
 orig_cities = set(ww_ref["city"])
 cities_3nf = set(ww["city_name"])
 
-print(orig_cities.difference(cities_3nf))
-print(cities_3nf.difference(orig_cities))
-ww_ref_eu = ww_ref[(ww_ref["city"] != 'Sarajevo')&(ww_ref["city"] != 'Bristol')&(ww_ref["city"] != 'London')]
+assert orig_cities.difference(cities_3nf) == cities_3nf.difference(orig_cities) == set()
+assert ww_ref.shape == ww.shape
 
-res = ww.merge(ww_ref_eu, how = "left", left_on =["city_name", "ref_year", "metabolite_name"], right_on = ["city", "year", "metabolite_name"] )
-matching = res.apply(lambda x: x.loc["daily_mean_concentration_x"] == x.loc["daily_mean_concentration_y"], axis = 1)
+rename_cols = {'city' : "city_name",
+               'year' : "ref_year",
+               'metabolite_name' : "metabolite_name",
+               'daily_mean_concentration' : "daily_mean_concentration"}
+ww_ref = ww_ref.rename(columns=rename_cols)
+ww_ref_prepped = ww_ref.sort_values(["city_name", "ref_year", "metabolite_name"]).reset_index(drop=True)
+ww_prepped = ww.sort_values(["city_name", "ref_year", "metabolite_name"]).reset_index(drop=True)
 
-print(res.loc[matching[matching == False].index, :])
+assert ww_prepped.compare(ww_ref_prepped, keep_equal=True).empty
