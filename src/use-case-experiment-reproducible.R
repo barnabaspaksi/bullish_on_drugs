1f480ad745a6f11069e72ad52107fa05366e8f75
barnabas.paksi <barnabas.paksi@zeiss.com>
2026-05-30 20:05:21 +0200

    added visuals to experiment script and reran it

--- src/use-case-experiment-reproducible.R
+++ src/use-case-experiment-reproducible.R
@@ -40,6 +40,49 @@ final_data <- final_ordered %>% pivot_wider(
 ) # we leave NA values bc 0 has true meaning of undetectable quantity
 
 #######################################
+
+# correlation plot
+cor_data <- final_data %>%
+  select(where(is.numeric)) %>%
+  cor(use = "pairwise.complete.obs")
+
+# plot as heatmap
+heatmap <- ggplot(data = melt(cor_data), aes(x=Var1, y=Var2, fill=value)) + 
+  geom_tile() +
+  scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
+                       midpoint = 0, limit = c(-1,1)) +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1)) +
+  labs(title = "Correlation: illicit drugs and regional GDP", 
+       fill = "Correlation", x = "", y="")
+heatmap
+ggsave("../outputs/correlation_heatmap.png")
+
+# histograms
+plot_data <- final_data %>%
+  select(where(is.numeric), -ref_year, -gdp) %>%
+  pivot_longer(cols = everything(), names_to = "variable", values_to = "value")
+
+# Create a multi-panel histogram
+hist_grams <- ggplot(plot_data, aes(x = value)) +
+  geom_histogram(bins = 30, fill = "darkgrey", color = "black") +
+  facet_wrap(~variable, scales = "free") +
+  theme_minimal() +
+  labs(title = "Distribution of Model Variables",
+       x = "(mg/1000p/day)",
+       y = "Frequency")
+hist_grams
+ggsave("../outputs/concentration_distrib_by_drug.png")
+
+# target variable histogram 
+p1 <- ggplot(final_data, aes(x = log(gdp))) + 
+  geom_histogram(fill="darkgrey", color="black", bins = 30) + 
+  theme_minimal() +
+  labs(title="Distribution of GDP Over Regions", x="Log(Euro)")
+p1
+ggsave("../outputs/overall_gdp_distribution.png")
+
+#######################################
 # split into training and test data
 
 set.seed(123, kind = "L'Ecuyer-CMRG")
